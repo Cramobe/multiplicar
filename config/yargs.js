0743c9c44c2a3b5d32ab46efc6d179e554002b7c
Cramobe Meitres <cristian.mosquera@meitres.com.co>
2020-04-20 16:46:36 -0500

    Commit inicial

---
+++ config/yargs.js
@@ -0,0 +1,22 @@
+const opts = {
+    base: {
+        demand: true,
+        alias: 'b',
+    },
+    limite: {
+        alias: 'l',
+        default: 10
+    }
+
+}
+
+const argv = require('yargs')
+                .command('listar', 'Imprime en consola la tabla de multiplicar',{opts})
+                .command('crear', 'Crea un archivo dado una base y un limite de tipo txt',{opts})
+                .help()
+                .argv;
+
+
+module.exports = {
+    argv
+}
